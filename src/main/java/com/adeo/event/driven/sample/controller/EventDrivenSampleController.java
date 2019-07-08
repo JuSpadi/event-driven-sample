5e4dbc461ea8c6e7d7bed32b70f50062feaef0c5
Frederic MAQUET <frederic.maquet@ext.adeo.com>
2019-07-08 14:30:36 +0200

    add body to send

--- src/main/java/com/adeo/event/driven/sample/controller/EventDrivenSampleController.java
+++ src/main/java/com/adeo/event/driven/sample/controller/EventDrivenSampleController.java
@@ -2,21 +2,22 @@ package com.adeo.event.driven.sample.controller;
 
 import java.io.IOException;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.adeo.event.driven.sample.SenderIn;
 import com.adeo.lys.event.context.ContextCreateOrUpdateInEvent;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @RequestMapping("/event-driven")
@@ -28,11 +29,9 @@ public class EventDrivenSampleController{
     private SenderIn sender;
     
     @PostMapping(value = "/send")
-    public ResponseEntity<Void> sendMessage(String messageContent) throws JsonParseException, JsonMappingException, IOException {
+    public ResponseEntity<Void> sendMessage(@RequestBody@Valid ContextCreateOrUpdateInEvent event) throws JsonParseException, JsonMappingException, IOException {
     	LOG.info("Creation message received");
-		ObjectMapper mapper = new ObjectMapper();
-		ClassPathResource sequenceRes = new ClassPathResource("/context.json");
-		sender.send(mapper.readValue(sequenceRes.getFile(), ContextCreateOrUpdateInEvent.class));
+		sender.send(event);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
